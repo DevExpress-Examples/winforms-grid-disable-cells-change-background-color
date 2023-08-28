9df33bc265f3ab35bf0d1f8adf1eb68afb86c9e3
DevExpressExampleBot <fakeEmail>
2023-08-28 13:42:06 +0400

    Source auto update [skip ci]

--- VB/MainForm.vb
+++ VB/MainForm.vb
@@ -1,40 +1,46 @@
-﻿Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.XtraGrid.Views.Grid
 Imports System
 Imports System.Windows.Forms
 
 Namespace DisabledCells
-	Partial Public Class MainForm
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-			FillDataSource()
-		End Sub
-		Private Sub FillDataSource()
-			dataTable1.Rows.Add("Product 1002", "US", True)
-			dataTable1.Rows.Add("Product 1001", "Germany", False)
-			dataTable1.Rows.Add("Product 1003", "Canada", True)
-		End Sub
-		Private Function IsShipToUSCanada(ByVal view As GridView, ByVal row As Integer) As Boolean
-			Try
-				Dim val As String = Convert.ToString(view.GetListSourceRowCellValue(row, "ShipCountry"))
-				Return (val = "US" OrElse val = "Canada")
-			Catch
-				Return False
-			End Try
-		End Function
-		Private Sub disabledCellEvents1_ProcessingCell(ByVal sender As Object, ByVal e As DevExpress.Utils.Behaviors.Common.ProcessCellEventArgs) Handles disabledCellEvents1.ProcessingCell
-			'Expression specified in the DisabledCellBehavior disables all row cells. Use the following code to enable cells in specific columns
-			If e.FieldName <> "Unbound" Then
-				e.Disabled = False
-			End If
-			'This code disables cells of the IsFreeShipping column depending on the IsShipToUSCanada condition
-			Dim view As GridView = TryCast(e.Source, GridView)
-			If e.FieldName = "IsFreeShipping" AndAlso IsShipToUSCanada(view, e.RecordId) Then
-				e.Disabled = True
-			End If
-		End Sub
-	End Class
+    Public Partial Class MainForm
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            FillDataSource()
+        End Sub
+
+        Private Sub FillDataSource()
+            dataTable1.Rows.Add("Product 1002", "US", True)
+            dataTable1.Rows.Add("Product 1001", "Germany", False)
+            dataTable1.Rows.Add("Product 1003", "Canada", True)
+        End Sub
+
+        Private Function IsShipToUSCanada(ByVal view As GridView, ByVal row As Integer) As Boolean
+            Try
+                Dim val As String = Convert.ToString(view.GetListSourceRowCellValue(row, "ShipCountry"))
+                Return Equals(val, "US") OrElse Equals(val, "Canada")
+            Catch
+                Return False
+            End Try
+        End Function
+
+        Private Sub disabledCellEvents1_ProcessingCell(ByVal sender As Object, ByVal e As DevExpress.Utils.Behaviors.Common.ProcessCellEventArgs)
+            'Expression specified in the DisabledCellBehavior disables all row cells. Use the following code to enable cells in specific columns
+            If Not Equals(e.FieldName, "Unbound") Then
+                e.Disabled = False
+            End If
+
+            'This code disables cells of the IsFreeShipping column depending on the IsShipToUSCanada condition
+            Dim view As GridView = TryCast(e.Source, GridView)
+            If Equals(e.FieldName, "IsFreeShipping") AndAlso IsShipToUSCanada(view, e.RecordId) Then
+                e.Disabled = True
+            End If
+        End Sub
+    End Class
 End Namespace
--- VB/Program.vb
+++ VB/Program.vb
@@ -1,24 +1,22 @@
-﻿Imports DevExpress.LookAndFeel
+Imports DevExpress.LookAndFeel
 Imports DevExpress.Skins
+Imports System
+Imports System.Windows.Forms
 
 Namespace DisabledCells
-    Friend NotInheritable Class Program
 
-        Private Sub New()
-        End Sub
+    Friend Module Program
 
         ''' <summary>
         ''' The main entry point for the application.
         ''' </summary>
-        <STAThread> _
-        Shared Sub Main()
-
-            Application.EnableVisualStyles()
+        <STAThread>
+        Sub Main()
+            Call Application.EnableVisualStyles()
             Application.SetCompatibleTextRenderingDefault(False)
-
-            SkinManager.EnableFormSkins()
+            Call SkinManager.EnableFormSkins()
             UserLookAndFeel.Default.SetSkinStyle("DevExpress Style")
-            Application.Run(New MainForm())
+            Call Application.Run(New MainForm())
         End Sub
-    End Class
+    End Module
 End Namespace
